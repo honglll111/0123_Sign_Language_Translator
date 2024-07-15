b76e85aecec7cae46343d3a99a4ec5c995613a07
honglll111 <87513761+honglll111@users.noreply.github.com>
2024-07-15 16:49:58 +0900

    Add files via upload

---
+++ SignLanguage/src/main/java/com/example/myapp/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.example.myapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer.defaultCodecs()
+                                .maxInMemorySize(-1)) // unlimited
+                        .build())
+                .baseUrl("http://localhost:5000")  // python AI 서버 주소
+                .build();
+    }
+}
