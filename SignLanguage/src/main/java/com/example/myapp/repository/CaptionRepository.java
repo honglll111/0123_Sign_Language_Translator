3c4d38bb65bcba37de524343e91b1d605f3a724f
hanbit <ohanbit6013@gmail.com>
2024-08-26 13:48:09 +0900

    클래스 삭제

--- SignLanguage/src/main/java/com/example/myapp/repository/CaptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.repository;
-
-import com.example.myapp.model.Caption;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CaptionRepository extends JpaRepository<Caption, Long> {
-}
